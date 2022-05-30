1039562838bfb1d37f4c137c74dea1524e32fa02
OsMemo05 <in.tanin@vamstack.com>
2022-05-30 16:09:28 +0700

    first commit

---
+++ industrial-video.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture("aravissrc ! videoconvert ! video/x-raw,format=BGR ! appsink", cv.CAP_GSTREAMER)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
